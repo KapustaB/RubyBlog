a853293473628bab6bbb188d28a7a72424c4ed94
bkapusta <kapusta.borna.s@gmail.com>
2017-10-26 11:40:43 +0200

    Added ENV secret keys for facebook api

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -250,7 +250,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-   config.omniauth :facebook, '1906578446272140', '73d43362c79ebd60debca431eda15d85'
+   config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET_KEY']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
